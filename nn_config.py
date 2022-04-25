5e930288ea6c3a65f7eb1734dc180f661fc8eeea
jdm365 <jdm365@georgetown.edu>
2022-04-24 20:24:08 -0400

    add nn_config file

---
+++ nn_config.py
@@ -0,0 +1,21 @@
+mlp_config = {
+        'fc1_dims': 1024,
+        'fc2_dims': 1024,
+        'fc3_dims': 1024,
+        'output_dims': 2
+    } 
+
+conv_config = {
+        'in_channels_1': 1,
+        'out_channels_1': 128,
+        'kernel_size_1': 4,
+        'in_channels_2': 128,
+        'out_channels_2': 128,
+        'kernel_size_2': 4,
+        'dropout_probs': 0.25,
+        'in_channels_3': 128,
+        'out_channels_3': 1,
+        'kernel_size_3': 20,
+        'fc1_dims': 1024,
+        'output_dims': 2
+    }
